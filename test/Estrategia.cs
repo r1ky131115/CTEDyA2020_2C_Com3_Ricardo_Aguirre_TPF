bc57c30572af84f59fe682cd5816747e6f7a1546
Ricardo Aguirre <ricardoaguirre_96@hotmail.com>
2020-11-09 17:30:30 -0300

    v1 tp final

---
+++ test/Estrategia.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+namespace DeepSpace
+{
+
+	class Estrategia
+	{
+		
+		
+		public String Consulta1( ArbolGeneral<Planeta> arbol)
+		{
+			return "Implementar";
+		}
+
+
+		public String Consulta2( ArbolGeneral<Planeta> arbol)
+		{
+			return "Implementar";
+		}
+
+
+		public String Consulta3( ArbolGeneral<Planeta> arbol)
+		{
+			return "Implementar";
+		}
+		
+		public Movimiento CalcularMovimiento(ArbolGeneral<Planeta> arbol)
+		{
+			//Implementar
+			
+			return null;
+		}
+	}
+}
